140b66ae664288fd53badc3d203860eef343870b
geofannyher <geofannyher@gmail.com>
2024-04-01 13:28:05 +0700

    fix

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,17 +2,13 @@ const LoadingComponent = () => {
   return (
     <>
       <div className="flex items-end gap-2">
-        <div className="relative flex">
+        <div className="relative flex h-6 w-6 items-center justify-center rounded-full border-2 text-black lg:h-10 lg:w-10">
           <img
             src={"https://picsum.photos/200/300"}
             className="h-10 w-10 items-center justify-center rounded-full object-cover"
           />
         </div>
-        <div className="lds-facebook w-40 rounded-br-xl rounded-tl-xl rounded-tr-xl bg-[#f2f6fa] p-4">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+        <div className="loader"></div>
       </div>
     </>
   );
