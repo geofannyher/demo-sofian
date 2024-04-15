5753a7667678b64a40ee86033a1d37d3928b183b
geofannyher <geofannyher@gmail.com>
2024-04-15 21:02:25 +0700

    sd

--- src/services/api/elevenlabs.service.ts
+++ src/services/api/elevenlabs.service.ts
@@ -9,7 +9,7 @@ export const textToSpeech = async (text: string) => {
         model_id: "eleven_multilingual_v2",
         voice_settings: {
           stability: 0.5,
-          similarity_boost: 0.75,
+          similarity_boost: 0.95,
           style: 0,
           use_speaker_boost: true,
         },
